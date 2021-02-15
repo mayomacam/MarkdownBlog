af1c123c1df8c707eadd9a06ce43632232d48c0c
mayomacam <amanrkt231217@gmail.com>
2021-02-15 09:16:07 +0530

    Finally completed

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple blog website
+
+I have used expressjs, nodejs , mongodb, Marked, dompurify, JSDOM, slugify.
+
+![](./blog.gif)
+
+>>>>> `DEBUG=backend:* npm start`
+for starting the webserver run this.
+
---
+++ routes/articles.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const Article = require('../models/article')
+const router = express.Router()
+
+router.get('/articles/new', (req, res) => {
+  res.render('articles/new', { article: new Article() })
+})
+
+router.get('/articles/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  res.render('articles/edit', { article: article })
+})
+
+router.get('/articles/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug })
+  if (article == null) res.redirect('/')
+  res.render('articles/show', { article: article })
+})
+
+router.post('/articles', async (req, res, next) => {
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/articles/:id', async (req, res, next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/articles/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    try {
+      article = await article.save()
+      res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article })
+    }
+  }
+}
+
+module.exports = router;
